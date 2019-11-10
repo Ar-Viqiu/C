c598dcff4710574d2cd9bd623f449eddd686596c
Ar-Viqiu <924557098@qq.com>
2019-11-10 15:50:56 +0800

    Create helooWorld1.cpp

---
+++ 2/helooWorld1.cpp
@@ -0,0 +1,4 @@
+#include <stdio.h>    //stdoi.h文件中包含了printf函数
+main (){      //书中给的是“void main(void)”，但这样写的话，实际上会被提示错误：main must return int ，用其他的写法就都可以，不知道是为什么。
+	printf("hello world!");   //printf函数向屏幕输出字符。
+} 
